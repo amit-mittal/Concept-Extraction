cf09a0b04e087de94cdfa6c601aeb699117e5551
amit-mittal <amit.mittal1993@gmail.com>
2015-03-23 19:50:53 +0530

    Code formatting changed

--- src/sample/criterias/Criteria.java
+++ src/sample/criterias/Criteria.java
@@ -2,48 +2,51 @@ package sample.criterias;
 
 public class Criteria
 {
-	private int true_positives;
-	private int true_negatives;
-	private int false_positives;
-	private int false_negatives;
-	
-	public Criteria(int tp, int tn, int fp, int fn)
-	{
-		this.true_positives = tp;
-		this.true_negatives = tn;
-		this.false_positives = fp;
-		this.false_negatives = fn;
-	}
-	
-	public float FindPrecision()
-	{
-		float p = (float)this.true_positives/(this.true_positives + this.false_positives);
-		
-		return p;
-	}
-	
-	public float FindRecall()
-	{
-		float r = (float)this.true_positives/(this.true_positives + this.false_negatives);
-		
-		return r;
-	}
-	
-	public float FindF1Measure()
-	{
-		float p = FindPrecision();
-		float r = FindRecall();
-		
-		float f1 = 2*p*r/(p+r);
-		
-		return f1;
-	}
-	
-	public float FindAccuracy()
-	{
-		int total = false_negatives + false_positives + true_negatives + true_positives;
-		float a = (float)(true_negatives + true_positives)/total;
-		
-		return a;
-	}
+    private int true_positives;
+    private int true_negatives;
+    private int false_positives;
+    private int false_negatives;
+
+    public Criteria(int tp, int tn, int fp, int fn)
+    {
+        this.true_positives = tp;
+        this.true_negatives = tn;
+        this.false_positives = fp;
+        this.false_negatives = fn;
+    }
+
+    public float FindPrecision()
+    {
+        float p = (float) this.true_positives
+                / (this.true_positives + this.false_positives);
+
+        return p;
+    }
+
+    public float FindRecall()
+    {
+        float r = (float) this.true_positives
+                / (this.true_positives + this.false_negatives);
+
+        return r;
+    }
+
+    public float FindF1Measure()
+    {
+        float p = FindPrecision();
+        float r = FindRecall();
+
+        float f1 = 2 * p * r / (p + r);
+
+        return f1;
+    }
+
+    public float FindAccuracy()
+    {
+        int total = false_negatives + false_positives + true_negatives
+                + true_positives;
+        float a = (float) (true_negatives + true_positives) / total;
+
+        return a;
+    }
 }
--- src/sample/randomindexing/RandomIndexer.java
+++ src/sample/randomindexing/RandomIndexer.java
@@ -49,255 +49,254 @@ import pitt.search.semanticvectors.VectorStoreWriter;
 
 public class RandomIndexer
 {
-	static Path INDEX_DIR = FileSystems.getDefault()
-			.getPath("positional_index");
-	static VectorStore newElementalTermVectors = null;
-	static TermTermVectorsFromLucene termTermIndexer;
-	static HashMap<Integer, String> idWordMap;
-	static HashMap<String, Integer> wordCategoryMap;
+    static Path INDEX_DIR = FileSystems.getDefault()
+            .getPath("positional_index");
+    static VectorStore newElementalTermVectors = null;
+    static TermTermVectorsFromLucene termTermIndexer;
+    static HashMap<Integer, String> idWordMap;
+    static HashMap<String, Integer> wordCategoryMap;
 
-	public static void main(String[] args)
-	{
-		String[] indexArgs = new String[5];
-		indexArgs[0] = "C:/Users/amit/Desktop/test/pubmed_result_001.txt";
-		indexArgs[1] = "-minfrequency";
-		indexArgs[2] = "2";
-		indexArgs[3] = "-maxnonalphabetchars";
-		indexArgs[4] = "3";
+    public static void main(String[] args)
+    {
+        // TODO try to optimize the parameters so that index size decreases
+        String[] indexArgs = new String[5];
+        indexArgs[0] = "C:/Users/amit/Desktop/test/pubmed_result_001.txt";
+        indexArgs[1] = "-minfrequency";
+        indexArgs[2] = "2";
+        indexArgs[3] = "-maxnonalphabetchars";
+        indexArgs[4] = "3";
 
-		//IndexFilePositions.main(indexArgs);
+        // IndexFilePositions.main(indexArgs);
 
-		String[] svArgs = new String[14];
-		svArgs[0] = "-vectortype";
-		svArgs[1] = "real";
-		svArgs[2] = "-dimension";
-		svArgs[3] = "100";
-		svArgs[4] = "-seedlength";
-		svArgs[5] = "15";
-		svArgs[6] = "-docindexing";
-		svArgs[7] = "incremental";
-		svArgs[8] = "-windowradius";
-		svArgs[9] = "2";
-		svArgs[10] = "-positionalmethod";
-		svArgs[11] = "directional";
-		svArgs[12] = "-luceneindexpath";
-		svArgs[13] = "positional_index/";
-		// svArgs[14] = "-indexfileformat";
-		// svArgs[15] = "text";
+        String[] svArgs = new String[14];
+        svArgs[0] = "-vectortype";
+        svArgs[1] = "real";
+        svArgs[2] = "-dimension";
+        svArgs[3] = "100";
+        svArgs[4] = "-seedlength";
+        svArgs[5] = "15";
+        svArgs[6] = "-docindexing";
+        svArgs[7] = "incremental";
+        svArgs[8] = "-windowradius";
+        svArgs[9] = "2";
+        svArgs[10] = "-positionalmethod";
+        svArgs[11] = "directional";
+        svArgs[12] = "-luceneindexpath";
+        svArgs[13] = "positional_index/";
+        // svArgs[14] = "-indexfileformat";
+        // svArgs[15] = "text";
 
-		// BuildPositionalIndex.main(svArgs);
+        // BuildPositionalIndex.main(svArgs);
 
-		BuildPositionalIndex(svArgs);
+        BuildPositionalIndex(svArgs);
 
-		// add query vector parameter so that can do for custom file
-		// also file format should be binary
-		String[] searchArgs = new String[3];
-		searchArgs[0] = "-queryvectorfile";
-		searchArgs[1] = "drxntermvectors.bin";
-		
-		// TODO Find F-Score and Accuracy to determine the best model
-		PopulateMaps();
-		FindNearestNeighbors(searchArgs);
-		tryStuff();
-		
-		// Then find the similarity matrix
-		GenerateSimilarityMatrix();
-	}
+        // add query vector parameter so that can do for custom file
+        // also file format should be binary
+        String[] searchArgs = new String[3];
+        searchArgs[0] = "-queryvectorfile";
+        searchArgs[1] = "drxntermvectors.bin";
 
-	static void BuildPositionalIndex(String[] args)
-			throws IllegalArgumentException
-	{
-		FlagConfig flagConfig;
-		try
-		{
-			flagConfig = FlagConfig.getFlagConfig(args);
-			args = flagConfig.remainingArgs;
-		}
-		catch (IllegalArgumentException e)
-		{
-			throw e;
-		}
+        PopulateMaps();
+        FindNearestNeighbors(searchArgs);
+        tryStuff();
 
-		if (flagConfig.luceneindexpath().isEmpty())
-		{
-			throw (new IllegalArgumentException("-luceneindexpath must be set."));
-		}
-		String luceneIndex = flagConfig.luceneindexpath();
+        // TODO Load UMLS
+        // TODO Find F-Score and Accuracy to determine the best model
 
-		String termFile = "";
-		switch (flagConfig.positionalmethod())
-		{
-		case BASIC:
-			termFile = flagConfig.termtermvectorsfile();
-			break;
-		case PROXIMITY:
-			termFile = flagConfig.proximityvectorfile();
-			break;
-		case PERMUTATION:
-			termFile = flagConfig.permutedvectorfile();
-			break;
-		case PERMUTATIONPLUSBASIC:
-			termFile = flagConfig.permplustermvectorfile();
-			break;
-		case DIRECTIONAL:
-			termFile = flagConfig.directionalvectorfile();
-			break;
-		default:
-			throw new IllegalArgumentException(
-					"Unrecognized -positionalmethod: "
-							+ flagConfig.positionalmethod());
-		}
+        // Then find the similarity matrix
+        // GenerateSimilarityMatrix();
+    }
 
-		System.out.println("Building positional index, Lucene index: "
-				+ luceneIndex + ", Seedlength: " + flagConfig.seedlength()
-				+ ", Vector length: " + flagConfig.dimension()
-				+ ", Vector type: " + flagConfig.vectortype()
-				+ ", Minimum term frequency: " + flagConfig.minfrequency()
-				+ ", Maximum term frequency: " + flagConfig.maxfrequency()
-				+ ", Number non-alphabet characters: "
-				+ flagConfig.maxnonalphabetchars() + ", Window radius: "
-				+ flagConfig.windowradius() + "\n");
+    static void BuildPositionalIndex(String[] args)
+            throws IllegalArgumentException
+    {
+        FlagConfig flagConfig;
+        try
+        {
+            flagConfig = FlagConfig.getFlagConfig(args);
+            args = flagConfig.remainingArgs;
+        }
+        catch (IllegalArgumentException e)
+        {
+            throw e;
+        }
 
-		try
-		{
-			termTermIndexer = new TermTermVectorsFromLucene(
-					flagConfig, newElementalTermVectors);
+        if (flagConfig.luceneindexpath().isEmpty())
+        {
+            throw (new IllegalArgumentException("-luceneindexpath must be set."));
+        }
+        String luceneIndex = flagConfig.luceneindexpath();
 
-			VectorStoreWriter.writeVectors(termFile, flagConfig,
-					termTermIndexer.getSemanticTermVectors());
+        String termFile = "";
+        switch (flagConfig.positionalmethod())
+        {
+        case BASIC:
+            termFile = flagConfig.termtermvectorsfile();
+            break;
+        case PROXIMITY:
+            termFile = flagConfig.proximityvectorfile();
+            break;
+        case PERMUTATION:
+            termFile = flagConfig.permutedvectorfile();
+            break;
+        case PERMUTATIONPLUSBASIC:
+            termFile = flagConfig.permplustermvectorfile();
+            break;
+        case DIRECTIONAL:
+            termFile = flagConfig.directionalvectorfile();
+            break;
+        default:
+            throw new IllegalArgumentException(
+                    "Unrecognized -positionalmethod: "
+                            + flagConfig.positionalmethod());
+        }
 
-			for (int i = 1; i < flagConfig.trainingcycles(); ++i)
-			{
-				newElementalTermVectors = termTermIndexer
-						.getSemanticTermVectors();
-				System.out
-						.println("\nRetraining with learned term vectors ...");
-				termTermIndexer = new TermTermVectorsFromLucene(flagConfig,
-						newElementalTermVectors);
-			}
+        System.out.println("Building positional index, Lucene index: "
+                + luceneIndex + ", Seedlength: " + flagConfig.seedlength()
+                + ", Vector length: " + flagConfig.dimension()
+                + ", Vector type: " + flagConfig.vectortype()
+                + ", Minimum term frequency: " + flagConfig.minfrequency()
+                + ", Maximum term frequency: " + flagConfig.maxfrequency()
+                + ", Number non-alphabet characters: "
+                + flagConfig.maxnonalphabetchars() + ", Window radius: "
+                + flagConfig.windowradius() + "\n");
 
-			// Incremental indexing is hardcoded into BuildPositionalIndex.
-			if (flagConfig.docindexing() != DocIndexingStrategy.NONE)
-			{
-				IncrementalDocVectors.createIncrementalDocVectors(
-						termTermIndexer.getSemanticTermVectors(), flagConfig,
-						new LuceneUtils(flagConfig));
-			}
-		}
-		catch (IOException e)
-		{
-			e.printStackTrace();
-		}
-	}
-	
-	public static void PopulateMaps()
-	{
-		idWordMap = new HashMap<Integer, String>();
-		// 1. problem 2. treatment 3. test 4. none
-		wordCategoryMap = new HashMap<String, Integer>(); 
-		Enumeration<ObjectVector> v = termTermIndexer.getSemanticTermVectors()
-				.getAllVectors();
-		
-		// Populating id to word map
-		int rank = 0;
-		while(v.hasMoreElements())
-		{
-			String word = v.nextElement().getObject().toString();
-			idWordMap.put(rank, word);
-			++rank;
-			
-			// if word this then assign this no.
-			wordCategoryMap.put(word, 0);
-		}
-	}
+        try
+        {
+            termTermIndexer = new TermTermVectorsFromLucene(flagConfig,
+                    newElementalTermVectors);
 
-	public static void FindNearestNeighbors(String[] args)
-	{
-		FlagConfig flagConfig;
-		List<SearchResult> results;
-		try
-		{
-			for(int i = 0 ; i < idWordMap.size() ; ++i)
-			{
-				args[2] = idWordMap.get(i);
-				flagConfig = FlagConfig.getFlagConfig(args);
-				results = Search.runSearch(flagConfig);
-				
-				int actualCategory = wordCategoryMap.get(args[2]);
-				double categories[] = new double[]{0.0, 0.0, 0.0, 0.0};
-				for (SearchResult result : results)
-				{
-					String w = result.getObjectVector().getObject().toString();
-					categories[wordCategoryMap.get(w)] += result.getScore();
-				}
-				
-				// find max likelihood category and then calculate mismatches etc.
-			}
-			
-		}
-		catch (IllegalArgumentException e)
-		{
-			throw e;
-		}
+            VectorStoreWriter.writeVectors(termFile, flagConfig,
+                    termTermIndexer.getSemanticTermVectors());
 
-		// Print out results.
-		/*int ranking = 0;
-		if (results.size() > 0)
-		{
-			System.out.println("Search output follows ...\n");
-			for (SearchResult result : results)
-			{
-				++ranking;
-				System.out.println(result.getObjectVector().getObject()
-						.toString());
+            for (int i = 1; i < flagConfig.trainingcycles(); ++i)
+            {
+                newElementalTermVectors = termTermIndexer
+                        .getSemanticTermVectors();
+                System.out
+                        .println("\nRetraining with learned term vectors ...");
+                termTermIndexer = new TermTermVectorsFromLucene(flagConfig,
+                        newElementalTermVectors);
+            }
 
-				
-				 * if (flagConfig.boundvectorfile().isEmpty() &&
-				 * flagConfig.elementalvectorfile().isEmpty()) {
-				 * PsiUtils.printNearestPredicate(flagConfig); }
-				 
-			}
+            // Incremental indexing is hardcoded into BuildPositionalIndex.
+            if (flagConfig.docindexing() != DocIndexingStrategy.NONE)
+            {
+                IncrementalDocVectors.createIncrementalDocVectors(
+                        termTermIndexer.getSemanticTermVectors(), flagConfig,
+                        new LuceneUtils(flagConfig));
+            }
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+    }
 
-			
-			 * if (!flagConfig.jsonfile().isEmpty()) {
-			 * PathFinder.pathfinderWriterWrapper(flagConfig, results); }
-			 
-		}
-		else
-		{
-			System.out.println("No search output.\n");
-		}*/
-	}
-	
-	public static void GenerateSimilarityMatrix()
-	{
-		Enumeration<ObjectVector> vi = termTermIndexer.getSemanticTermVectors()
-				.getAllVectors();
-		int size = termTermIndexer.getSemanticTermVectors().getNumVectors();
-		
-		// TODO optimize this
-		double[][] matrix = new double[size][size];
-		for(int i = 0 ; i < size ; ++i)
-		{
-			ObjectVector m = vi.nextElement();
-			Enumeration<ObjectVector> vj = termTermIndexer.getSemanticTermVectors()
-					.getAllVectors();
-			for(int j = 0 ; j < size ; ++j)
-			{
-				ObjectVector n = vj.nextElement();
-				matrix[i][j] = m.getVector().measureOverlap(n.getVector());
-			}
-		}
-	}
+    public static void PopulateMaps()
+    {
+        idWordMap = new HashMap<Integer, String>();
+        // 1. problem 2. treatment 3. test 4. none
+        wordCategoryMap = new HashMap<String, Integer>();
+        Enumeration<ObjectVector> v = termTermIndexer.getSemanticTermVectors()
+                .getAllVectors();
 
-	public static void tryStuff()
-	{
-		Enumeration<ObjectVector> v = termTermIndexer.getSemanticTermVectors()
-				.getAllVectors();
-		System.out.println(termTermIndexer.getSemanticTermVectors().getVector(
-				"methodological"));
-		
-		System.out.println(v.nextElement().getObject()); // this gets the tag value
-		System.out.println(v.nextElement().getVector()); // this gets the vector
-	}
+        // Populating id to word map
+        int rank = 0;
+        while (v.hasMoreElements())
+        {
+            String word = v.nextElement().getObject().toString();
+            idWordMap.put(rank, word);
+            ++rank;
+
+            // if word this then assign this no.
+            wordCategoryMap.put(word, 0);
+        }
+    }
+
+    public static void FindNearestNeighbors(String[] args)
+    {
+        FlagConfig flagConfig;
+        List<SearchResult> results;
+        try
+        {
+            for (int i = 0; i < idWordMap.size(); ++i)
+            {
+                args[2] = idWordMap.get(i);
+                flagConfig = FlagConfig.getFlagConfig(args);
+                results = Search.runSearch(flagConfig);
+
+                int actualCategory = wordCategoryMap.get(args[2]);
+                double categories[] = new double[]
+                { 0.0, 0.0, 0.0, 0.0 };
+                for (SearchResult result : results)
+                {
+                    String w = result.getObjectVector().getObject().toString();
+                    categories[wordCategoryMap.get(w)] += result.getScore();
+                }
+
+                // find max likelihood category and then calculate mismatches
+                // etc.
+            }
+
+        }
+        catch (IllegalArgumentException e)
+        {
+            throw e;
+        }
+
+        // Print out results.
+        /*
+         * int ranking = 0; if (results.size() > 0) {
+         * System.out.println("Search output follows ...\n"); for (SearchResult
+         * result : results) { ++ranking;
+         * System.out.println(result.getObjectVector().getObject() .toString());
+         * 
+         * 
+         * if (flagConfig.boundvectorfile().isEmpty() &&
+         * flagConfig.elementalvectorfile().isEmpty()) {
+         * PsiUtils.printNearestPredicate(flagConfig); }
+         * 
+         * }
+         * 
+         * 
+         * if (!flagConfig.jsonfile().isEmpty()) {
+         * PathFinder.pathfinderWriterWrapper(flagConfig, results); }
+         * 
+         * } else { System.out.println("No search output.\n"); }
+         */
+    }
+
+    public static void GenerateSimilarityMatrix()
+    {
+        Enumeration<ObjectVector> vi = termTermIndexer.getSemanticTermVectors()
+                .getAllVectors();
+        int size = termTermIndexer.getSemanticTermVectors().getNumVectors();
+
+        // TODO optimize this
+        double[][] matrix = new double[size][size];
+        for (int i = 0; i < size; ++i)
+        {
+            ObjectVector m = vi.nextElement();
+            Enumeration<ObjectVector> vj = termTermIndexer
+                    .getSemanticTermVectors().getAllVectors();
+            for (int j = 0; j < size; ++j)
+            {
+                ObjectVector n = vj.nextElement();
+                matrix[i][j] = m.getVector().measureOverlap(n.getVector());
+            }
+        }
+    }
+
+    public static void tryStuff()
+    {
+        Enumeration<ObjectVector> v = termTermIndexer.getSemanticTermVectors()
+                .getAllVectors();
+        System.out.println(termTermIndexer.getSemanticTermVectors().getVector(
+                "methodological"));
+
+        System.out.println(v.nextElement().getObject()); // this gets the tag
+                                                         // value
+        System.out.println(v.nextElement().getVector()); // this gets the vector
+    }
 }
