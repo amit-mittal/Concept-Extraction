b480a26d687b8d0b7f065f6c4533350155c6e091
amit-mittal <amit.mittal1993@gmail.com>
2015-04-04 00:34:35 +0530

    Bugs in pipe fixed

--- src/sample/i2b2/ThesaurusPipe.java
+++ src/sample/i2b2/ThesaurusPipe.java
@@ -11,10 +11,11 @@ import edu.umass.cs.mallet.base.pipe.Pipe;
 import edu.umass.cs.mallet.base.types.Instance;
 import edu.umass.cs.mallet.base.types.Token;
 import edu.umass.cs.mallet.base.types.TokenSequence;
-import edu.umass.cs.mallet.base.util.PropertyList;
 
 public class ThesaurusPipe extends Pipe implements Serializable
 {
+    private static final long serialVersionUID = 79533921516L;
+    
     String prefix;
     int leftBoundary;
     int rightBoundary;
@@ -37,17 +38,27 @@ public class ThesaurusPipe extends Pipe implements Serializable
     {
         TokenSequence ts = (TokenSequence) carrier.getData();
         int tsSize = ts.size();
-        PropertyList[] newFeatures = new PropertyList[tsSize];
         
         for (int i = 0; i < tsSize; i++)
         {
             Token t = (Token) ts.get(i);
-            PropertyList pl = t.getFeatures();
-            newFeatures[i] = pl;
             
             Vector v1 = null;
             if(storeReader.containsVector(t.getText())){
                 v1 = storeReader.getVector(t.getText());
+            }
+            
+            if(v1 == null){
+                for (int position = i + leftBoundary; position < i + rightBoundary; position++)
+                {
+                    int index = position - i;
+                    if (position == i)
+                        continue;
+
+                    t.setNumericProperty(prefix + index, 0.0);
+                }
+                
+                continue;
             }
             
             for (int position = i + leftBoundary; position < i + rightBoundary; position++)
@@ -58,28 +69,19 @@ public class ThesaurusPipe extends Pipe implements Serializable
 
                 if (position < 0 || position >= tsSize)
                 {
-                    newFeatures[i] = PropertyList
-                            .add(prefix + index + "=", 0.0, newFeatures[i]);
+                    t.setNumericProperty(prefix + index, 0.0);
                 }
                 else{
                     Token e = (Token) ts.get(position);
                     if(storeReader.containsVector(e.getText())){
                         Vector v2 = storeReader.getVector(e.getText());
-                        newFeatures[i] = PropertyList
-                                .add(prefix + index + "=", v1.measureOverlap(v2), newFeatures[i]);
+                        t.setNumericProperty(prefix + index, v1.measureOverlap(v2));
                     }
                     else{
-                        newFeatures[i] = PropertyList
-                                .add(prefix + index + "=", 0.0, newFeatures[i]);
+                        t.setNumericProperty(prefix + index, 0.0);
                     }
                 }
             }
-        }
-        
-        for (int i = 0; i < tsSize; i++)
-        {
-            // Put the new PropertyLists in place
-            ((Token) ts.get(i)).setFeatures(newFeatures[i]);
         }
         
         return carrier;
