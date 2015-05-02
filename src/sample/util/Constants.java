91321a3370ce8e93a1b0adf823899720fa2618d0
amit-mittal <amit.mittal1993@gmail.com>
2015-05-02 13:34:52 +0530

    Changes in the path of resource files

--- src/sample/util/Constants.java
+++ src/sample/util/Constants.java
@@ -6,13 +6,26 @@ public class Constants
     
     public static String POSITIONAL_INDEX_FOLDER_PATH = "resources/positional_index/";
     
-    public static String WORD_VECTORS_FILE_PATH = "drxntermvectors.bin";
-    
-    public static String i2b2_INDEX_FOLDER_PATH = "C:/Users/amit/workspace/Concept-Extraction/index";
+    public static String WORD_VECTORS_FILE_PATH = "resources/word_vectors/drxntermvectors.bin";
     
     public static String WORD_LIST_FILE_PATH = "resources/binary/wordlist";
     
     public static String SIMILARITY_MATRIX_FILE_PATH = "resources/binary/similarity_matrix";
+    
+    public static String PROBLEMS_DICTIONARY_FILE_PATH = "resources/dictionaries/problems.txt";
+    
+    public static String TREATMENTS_DICTIONARY_FILE_PATH = "resources/dictionaries/treatments.txt";
+    
+    public static String TESTS_DICTIONARY_FILE_PATH = "resources/dictionaries/tests.txt";
+    
+    public static String SQL_DATABASE_PATH = "jdbc:mysql://localhost/2014ab";
+    
+    public static String SQL_USERNAME = "root";
+    
+    public static String SQL_PASSWORD = "";
+    
+    // TRANING FOLDER
+    public static String i2b2_INDEX_FOLDER_PATH = "C:/Users/amit/workspace/Concept-Extraction/index";
     
     public static String i2b2_DATA_TRAINING_FOLDER_PATH = "C:/Users/amit/Desktop/txt/";
     
@@ -20,19 +33,10 @@ public class Constants
     
     public static String i2b2_MALLET_TRAINING_FOLDER_PATH = "C:/Users/amit/Desktop/newconcept/";
     
+    // TEST FOLDER
+    public static String i2b2_MALLET_TEST_FOLDER_PATH = "C:/Users/amit/Desktop/newconcept_p/";
+
+    // LOCATION OF CRF MODEL
     public static String CRF_MODEL_FILE_PATH = "C:/Users/amit/Desktop/CRF1.txt";
     
-    public static String i2b2_MALLET_TEST_FOLDER_PATH = "C:/Users/amit/Desktop/newconcept_p/";
-    
-    public static String PROBLEMS_DICTIONARY_FILE_PATH = "C:/Users/amit/Desktop/problems.txt";
-    
-    public static String TREATMENTS_DICTIONARY_FILE_PATH = "C:/Users/amit/Desktop/treatments.txt";
-    
-    public static String TESTS_DICTIONARY_FILE_PATH = "C:/Users/amit/Desktop/tests.txt";
-    
-    public static String SQL_DATABASE_PATH = "jdbc:mysql://localhost/2014ab";
-    
-    public static String SQL_USERNAME = "root";
-    
-    public static String SQL_PASSWORD = "";
 }
